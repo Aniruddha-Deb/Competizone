0de282291aa40b15dedccb0a2633ce58944f2b0a
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2022-06-19 11:06:30 +0530

    1695C

--- old/codeforces/1695/1695C.cpp
+++ old/codeforces/1695/1695C.cpp
@@ -31,29 +31,20 @@ void solve() {
 		return;
 	}
 
-	vector<vi> DP(n,vi(m,0));
-	DP[0][0] = A[0][0];
-	rep(i,1,n) DP[i][0] = DP[i-1][0] + A[i][0];
-	rep(i,1,m) DP[0][i] = DP[0][i-1] + A[0][i];
+	vector<vi> DP_l(n,vi(m,0));
+	vector<vi> DP_u(n,vi(m,0));
+	DP_l[0][0] = DP_u[0][0] = A[0][0];
+	rep(i,1,n) DP_l[i][0] = DP_u[i][0] = DP_l[i-1][0] + A[i][0];
+	rep(i,1,m) DP_l[0][i] = DP_u[0][i] = DP_l[0][i-1] + A[0][i];
 
 	rep(i,1,n) {
 		rep(j,1,m) {
-			int o1 = DP[i-1][j] + A[i][j];
-			int o2 = DP[i][j-1] + A[i][j];
-			if (o2 >= 0 && o1 >= 0) {
-				DP[i][j] = min(o1,o2);
-			}
-			else if (((ll)o2)*o1 < 0) {
-				if (abs(o2) > abs(o1)) DP[i][j] = o1;
-				else DP[i][j] = o2;
-			}
-			else {
-				DP[i][j] = max(o1,o2);
-			}
+			DP_l[i][j] = A[i][j] + min(DP_l[i-1][j],DP_l[i][j-1]);
+			DP_u[i][j] = A[i][j] + max(DP_u[i-1][j],DP_u[i][j-1]);
 		}
 	}
 
-	if (DP[n-1][m-1] == 0) cout << "YES" << endl;
+	if (((ll)DP_l[n-1][m-1])*DP_u[n-1][m-1] <= 0) cout << "YES" << endl;
 	else cout << "NO" << endl;
 }
 
